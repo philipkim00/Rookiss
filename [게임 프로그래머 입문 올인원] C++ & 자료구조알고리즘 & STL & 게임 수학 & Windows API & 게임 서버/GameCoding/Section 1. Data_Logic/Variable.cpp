143329eb5993601b58476f01bcc4bf14db6a85b4
김민혁 <philipkim00@gmail.com>
2024-05-28 21:23:39 +0900

    Variable(변수) 코드 + 변수명 짓기

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/Variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// [타입] [이름];
+
+int hp; // 영어 가능
+int Hp; // 대소문자 구분 가능
+int hp_1; // _사용가능
+// int hp_한글; // 변수명 한글 x
+
+int main()
+{
+	std::cout << "Hello World\n";
+	
+	return 0;
+}
