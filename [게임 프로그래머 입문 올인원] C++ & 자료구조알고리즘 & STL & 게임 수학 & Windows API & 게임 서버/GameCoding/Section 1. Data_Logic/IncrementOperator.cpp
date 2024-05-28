f7f41dcd5240c049b9f562d6be4a7c50a8049fdb
김민혁 <philipkim00@gmail.com>
2024-05-28 23:18:58 +0900

    IncrementOperator(증감연산자)코드

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/IncrementOperator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int hp;
+int a;
+
+int main()
+{
+    hp = 100;
+    // 1
+    //hp = hp + 1;
+    // 2
+    //hp += 1;
+    // 3
+    //hp++;
+    // 4
+    //++hp;
+
+    // 5
+    cout << a++ << endl;
+
+    // 6
+    cout << ++a << endl;
+}
