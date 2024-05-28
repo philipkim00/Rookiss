e875ab7d11fe1a91b48b815902de9826754b923a
김민혁 <philipkim00@gmail.com>
2024-05-28 23:06:27 +0900

    FourBasicOperations(사칙 연산, 네가지라고 코드명을 적었는데 5가지임) 코드

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/FourBasicOperations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// [타입] [이름];
+
+int a;
+int b;
+
+int main()
+{
+	// +, -, *, /, %
+	a = 10;
+	b = 3;
+
+	cout << (a + b) << endl;
+	cout << (a - b) << endl;
+	cout << (a * b) << endl;
+	cout << (a / b) << endl;
+	cout << (a % b) << endl;
+}
