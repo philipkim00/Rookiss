bded76c94519ae7c3893bb1c024aa3e38d3d6e3b
김민혁 <philipkim00@gmail.com>
2024-05-28 22:30:36 +0900

    LossInformation(정보의 손실,데이터 손실) 코드

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/LossInformation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// [타입] [이름];
+
+// char : 1
+// short : 2
+// int : 4
+// __int64(long long) : 8
+
+int hp;
+short mp;
+
+int main()
+{
+	hp = 306189112;
+	
+	mp = hp;
+
+	cout << mp;
+}
