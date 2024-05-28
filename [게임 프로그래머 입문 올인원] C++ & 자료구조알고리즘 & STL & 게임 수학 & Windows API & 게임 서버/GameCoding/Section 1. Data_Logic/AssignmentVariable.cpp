a5923c947c4ee84251a6953d63c13b667e6d60af
김민혁 <philipkim00@gmail.com>
2024-05-28 21:48:35 +0900

    AssignmentVariable(변수에 값 대입) 코드

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/AssignmentVariable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// [타입] [이름];
+
+// char : 1
+// short : 2
+// int : 4
+// __int64(long long) : 8
+int hp;
+
+int main()
+{
+	hp = 50;
+	
+	//std::cout << "Hello World\n";
+}
