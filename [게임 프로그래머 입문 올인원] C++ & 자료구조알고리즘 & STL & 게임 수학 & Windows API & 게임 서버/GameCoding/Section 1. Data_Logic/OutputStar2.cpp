3921822fc890416f054c54f49ab605f8ca927efe
김민혁 <philipkim00@gmail.com>
2024-05-30 22:32:25 +0900

    OutputStar (직각삼각형) 코드

---
+++ [게임 프로그래머 입문 올인원] C++ & 자료구조알고리즘 & STL & 게임 수학 & Windows API & 게임 서버/GameCoding/Section 1. Data_Logic/OutputStar2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int j = 0; j < N; j++)
+	{
+		for (int i = 0; i < N - j; i++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+}
